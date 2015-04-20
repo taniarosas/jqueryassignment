0344e04d21372c47393f70d129400aa5e50b07c8
tania rosas-jimenez <rosastania@yahoo.com>
2015-04-20 12:05:31 -0700

    finished test

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('div').click(function() {
+		alert("Hey you clicked on me");
+	});
+});
+$(document).ready(function() {
+	$('#iwao').click(function() {
+		alert("Hey you clicked on me again");
+	});
+});
